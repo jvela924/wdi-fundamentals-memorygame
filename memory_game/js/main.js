fc860df85d6d469c177d82dc5a7d73257870735a
Jeremy Vela <jeremyvela@Jeremys-MBP.home>
2019-06-17 12:31:24 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,21 +25,53 @@ var cardsInPlay = []
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
-}
+  		alert("You found a match!");
+
+	} else {
+  		alert("Sorry, try again.");
+	}
 }
 
-function flipCard (cardId) {
+function flipCard () {
+	var cardId = this.getAttribute("data-id");
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute("src", cards[cardId].cardImage)
 	if (cardsInPlay.length == 2) {
 		checkForMatch();
 	}
 	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i=0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images-2/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+createBoard();
+shuffle(cards);
